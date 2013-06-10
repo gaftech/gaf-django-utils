0b8fdbc2f676087f858f96243a47414c0b3ec925
Gabriel <gabriel@gaftech.fr>
2013-06-10 10:45:32 +0200

    add extended base command class

--- gafutils/commands.py
+++ gafutils/commands.py
@@ -12,6 +12,7 @@ import os
 from django.core.management.base import CommandError
 from subprocess import check_call
 import shlex
+import logging
 
 class SudoCommand(BaseCommand):
     """
@@ -34,4 +35,54 @@ class SudoCommand(BaseCommand):
         
         self.sudo_handle(*args, **options)
         
+class ExtendedBaseCommand(BaseCommand):
+    """
+    Extended command class
+    """
     
+    logger_name = None
+    
+    def handle(self, *args, **options):
+        self.init_logging(*args, **options)
+        return self.do_handle(*args, **options)
+        
+        
+        
+    def init_logging(self, *args, **options):
+        self.logger = logging.getLogger(self.get_logger_name())
+        if options['verbosity'] == "3":
+            logging.getLogger().setLevel(logging.DEBUG)    
+    
+    def get_logger_name(self):
+        if self.logger_name is not None:
+            return self.logger_name
+        module = self.__class__.__module__.split('.')
+        cmd_name = module[-1]
+        app_name = module[-4]
+        
+        return "%s.cmd.%s" % (app_name, cmd_name)
+        
+    def do_handle(self, *args, **options):
+        raise NotImplementedError(u"Subclasses must override ExtendedBaseCommand.do_handle()")
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
